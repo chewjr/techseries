8e6b3faf69719ee88f6bfea6f87d1c6334c08100
denysetan <54625232+denysetan@users.noreply.github.com>
2020-09-04 22:04:55 +0800

    Create database

---
+++ app2/techseries.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Sep 04, 2020 at 02:02 PM
+-- Server version: 5.7.23
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `techseries`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `budget`
+--
+
+DROP TABLE IF EXISTS `budget`;
+CREATE TABLE IF NOT EXISTS `budget` (
+  `user_id` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `amount` float NOT NULL,
+  PRIMARY KEY (`user_id`,`created_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `expenses`
+--
+
+DROP TABLE IF EXISTS `expenses`;
+CREATE TABLE IF NOT EXISTS `expenses` (
+  `user_id` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `amount` float NOT NULL,
+  PRIMARY KEY (`user_id`,`created_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `goal`
+--
+
+DROP TABLE IF EXISTS `goal`;
+CREATE TABLE IF NOT EXISTS `goal` (
+  `user_id` varchar(20) NOT NULL,
+  `goal_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `category` varchar(30) NOT NULL,
+  `amount` float NOT NULL,
+  `deadline` datetime NOT NULL,
+  PRIMARY KEY (`user_id`,`goal_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `occupation` varchar(20) NOT NULL,
+  `age` int(11) NOT NULL,
+  `risk` float NOT NULL,
+  `email` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
